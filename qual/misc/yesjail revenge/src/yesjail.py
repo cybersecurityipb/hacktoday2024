2f8ee3c2f14c7d7af5162dfa39d73e7623adda23
zran147 <muhammadzahran@apps.ipb.ac.id>
2024-08-24 13:45:40 +0700

    ngubah yesjail revenge

--- qual/misc/yesjail revenge/src/yesjail.py
+++ qual/misc/yesjail revenge/src/yesjail.py
@@ -10,7 +10,7 @@ while jail[-1] != 'EOF':
     jail.append(input())
 jail = '\n'.join(jail[:-1])
 
-if '__' in jail or any([c for c in jail if c in string.whitespace.replace('\n', '')+'(){}@']) or not all(ord(c) >= 0 and ord(c) <= 255 for c in jail):
+if '__' in jail or any([c for c in jail if c in string.whitespace.replace('\n', '')+'(){}@#']) or not all(ord(c) >= 0 and ord(c) <= 255 for c in jail):
     print('yes?')
     exit()
 
