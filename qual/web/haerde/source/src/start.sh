4424a8d9f0d35af664c3589c4cca2bc313536752
UnoArroefy <numroua@gmail.com>
2024-08-24 10:53:53 +0700

    db issue

--- qual/web/haerde/source/src/start.sh
+++ qual/web/haerde/source/src/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+su postgres -c "/usr/lib/postgresql/15/bin/postgres -D /var/lib/postgresql/data start"
 su postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD '${POSTGRES_PASSWORD}';\""
-su postgres -c "/usr/lib/postgresql/15/bin/postgres -D /var/lib/postgresql/data &"
 sleep 5
 python3 app.py
